a3b2fd60344f5411bfceb43a82b776a189a0872e
Knguyen2003 <nguyenphan1122k3@gmail.com>
2025-03-20 12:11:53 +0700

    Tuan05_design pattern

---
+++ Tuan05/design-pattern/observer/src/main/java/com/iuh/Subject.java
@@ -0,0 +1,12 @@
+package com.iuh;
+
+public interface Subject {
+
+    public void register(Observer obj);
+    public void unregister(Observer obj);
+
+    //Khi trạng thái của Subject thay đổi, phương thức này sẽ được gọi để thông báo cho tất cả các Observer cập nhật thông tin.
+    public void notifyObservers();
+    // Thay vì Subject tự động gửi dữ liệu khi thông báo, Observer có thể chủ động gọi phương thức này để lấy thông tin.
+    public Object getUpdate(Observer obj);
+}
